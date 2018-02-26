24e39daf6a86005a671d4699816977ae37c74663
adeel khilji <adeel.khilji83@gmail.com>
2018-02-26 00:07:15 -0500

    first commit

---
+++ Assets-for-React-Course/01-playground/example1.js
@@ -0,0 +1,37 @@
+class AppComponent extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {count: 5, title: "Hello World"};
+
+        this.increment = this.increment.bind(this);
+        this.decrement = this.decrement.bind(this);
+    }
+    render(){
+        const {count, title} = this.state;
+        return(
+            <section className="site-wrap">
+                <h1>Header: {title}!</h1>
+                <p>the Counter is: {count}!</p>
+                <div>
+                    <button onClick={this.decrement}>-</button>
+                    <button onClick={this.increment} >+</button>
+                </div>
+            </section>
+        );
+    }
+
+    increment() {
+        
+        const {count} = this.state;
+        this.setState({count: count + 1, title:"INC"});
+    }
+    decrement(){
+        const {count} = this.state;
+        this.setState({count: count - 1, title:"DEC"});
+    }
+}
+
+ReactDOM.render(
+    <AppComponent/>,
+    document.getElementById("application")
+);
